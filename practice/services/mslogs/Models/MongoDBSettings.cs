8b26392b5cf600f625e62547b7bd6f7ac054a547
diego.val.vel.cursos <diego.val.vel.cursos@outlook.com>
2024-08-27 08:23:02 -0600

    deleted for practice

--- practice/services/mslogs/Models/MongoDBSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace Practice.Services.mslogs.Models
-{
-    public class MongoDBSettings
-    {
-        public string ConnectionString { get; set; } = null!;
-        public string DatabaseName { get; set; } = null!;
-        public string UsersCollectionName { get; set; } = null!;
-    }
-}
